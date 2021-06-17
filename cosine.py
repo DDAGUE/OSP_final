797c950fd1fdaa05839b9f6264dc41793eb8a41f
komplamoose <78531103+komplamoose@users.noreply.github.com>
2021-06-17 16:33:21 +0900

    Update cosine.py

--- cosine.py
+++ cosine.py
@@ -3,6 +3,8 @@
 import math
 import re
 from collections import Counter
+from bs4 import BeautifulSoup
+import requests
 
 def get_cosine(vec1, vec2):i #코사인 유사도값 반환 함수
     intersection = set(vec1.keys()) & set(vec2.keys())
@@ -18,11 +20,23 @@ def get_cosine(vec1, vec2):i #코사인 유사도값 반환 함수
         return float(numerator) / denominator
 
 
-def text_to_vector(point):#주어진 리스트를 벡터화 시키는 함수
+def list_to_vector(point):#주어진 리스트를 벡터화 시키는 함수
     return Counter(point)
 
 
 def main_cosine(origin): #코사인 유사도 구하는 메인 함수, 주어진 movie의 특징을 딕셔너리로 입력받음. key값은 영화 이름, value값은 영화 특징들로 이뤄진 리스트 형태
+    og_list #원래 오리진 url에서 받아온 영화 특징으로 이뤄진 리스트
+    og_v = list_to_vector(og_list) #og_list를 벡터화한 값
+
+    movie_2000 = dict() #2000개의 영화를 저장할 딕셔너리, key값 : 영화 이름, value값 : 영화 특징 리스트
+    cos_2000 = dict() #2000개의 영화의 코사인 유사도값 저장해둘 딕셔너리, key값 : 영화 이름, value값 : og와 비교한 코사인 유사도 값
+    
+
+    #만든 딕셔너리 정렬
+
+    #탑10 리턴
+
+
     #구현해야 할 요소
     #1. 엘라스틱 서치에 입력된 영화 데이터들을 딕셔너리 형태로 끌어오기, {영화 이름:'영화 특징 리스트'}형태로 2000개 끌어오기.
     #2. {영화 이름:코사인 유사도 값}으로 딕셔너리 하나 더 생성, 나중에 유사도 값 정렬하고 비교하기 위함}
