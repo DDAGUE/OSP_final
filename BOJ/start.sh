58750cbc9683080037178f9718d4c75d6d006352
DDAGUE <31721315+DDAGUE@users.noreply.github.com>
2021-06-23 14:30:30 +0900

    Create start.sh

---
+++ BOJ/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip3 install re
+pip3 install requests
+pip3 install flask
+pip3 install bs4
+pip3 install math
+pip3 install collections
+pip3 install elasticsearch
+pip3 install sklearn
+python3 app.py
